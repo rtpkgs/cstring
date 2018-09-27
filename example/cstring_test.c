0472d6b91e1d54bbe7992a0e33c23c659c0b691a
流光 <1004383796@qq.com>
2018-09-27 10:44:19 +0800

    [更新] 添加cstring例程.

---
+++ example/cstring_test.c
@@ -0,0 +1,27 @@
+#include "cstring.h"
+#include <stdio.h>
+#include "rtthread.h" 
+#include "finsh.h"
+
+static cstring foo(cstring t){
+	CSTRING_LITERAL(hello, "hello");
+	CSTRING_BUFFER(ret);
+	if (cstring_equal(hello, t))
+		cstring_cat(ret, "equal");
+	else
+		cstring_cat(ret, "not equal");
+	return cstring_grab(CSTRING(ret));
+}
+
+static void test(void){
+	printf("buildpkg.\n"); 
+	CSTRING_BUFFER(a);	
+	cstring_printf(a, "%s", "hello");
+	cstring b = foo(CSTRING(a));
+	printf("%s\n", b->cstr);
+	cstring_printf(a, "very long string %01024d", 0);
+	printf("%s\n", CSTRING(a)->cstr);
+	CSTRING_CLOSE(a);
+	cstring_release(b);
+}
+MSH_CMD_EXPORT_ALIAS(test, test, cstring test.); 
